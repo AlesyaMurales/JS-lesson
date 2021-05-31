537b404ef9021ad4fa1ab1e01b425536804e709a
Alesya <alesyamurales@gmail.com>
2021-06-01 00:57:48 +0500

    Lesson2

---
+++ lesson2/5/5.js
@@ -0,0 +1,22 @@
+function addition(a, b) {
+    return a + b;
+};
+console.log(addition(3, 3));
+
+
+function subtraction(a, b) {
+    return a - b;
+};
+console.log(subtraction(3, 3));
+
+
+function division(a, b) {
+    return a / b;
+};
+console.log(division(3, 3));
+
+
+function multiplication(a, b) {
+    return a * b;
+};
+console.log(multiplication(3, 3));
